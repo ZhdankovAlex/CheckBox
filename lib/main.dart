92ae8f4962cd8900961f4829f11e0039f4d3bb0b
ZhdankovAlex <47736968+ZhdankovAlex@users.noreply.github.com>
2020-08-07 15:48:42 +0300

    Header of dropdown list updates every time, but flag not updates (I need to reopen main panel)

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(home: MyAppState()));
+void main() => runApp(MaterialApp(home: MyApp()));
 
-class MyAppState extends StatefulWidget
-{
+class MyApp extends StatelessWidget{
   @override
-  MyApp createState() => MyApp();
-}
-
-class MyApp extends State<MyAppState>
-{
-  @override
-  Widget build(BuildContext context)
-  {
+  Widget build(BuildContext context)  {
     return MaterialApp
     (
       title: 'Dropdown checkbox',
@@ -29,31 +21,33 @@ class MyApp extends State<MyAppState>
   }
 }
 
-class CustomDropdown extends StatefulWidget
-{
+class CustomDropdown extends StatefulWidget{
   String text;
   CustomDropdown({Key key, this.text = "Язык"}): super(key: key);
+
   @override
   _CustomDropdownState createState() => _CustomDropdownState();
 }
 
-class _CustomDropdownState extends State<CustomDropdown>
-{
+class _CustomDropdownState extends State<CustomDropdown> {
   GlobalKey actionKey;
+
   bool isDropdownOpened = false;
   OverlayEntry floatingDropdown;
-  IconData shevron = IconData(0xe5cc, fontFamily: 'MaterialIcons', matchTextDirection: true);
+  IconData shevron = IconData(
+      0xe5cc, fontFamily: 'MaterialIcons', matchTextDirection: true);
   double height, width, xPosition, yPosition;
 
+  bool isSelectedEnglish = false;
+  bool isSelectedRussian = true;
+
   @override
-  void initState()
-  {
+  void initState() {
     actionKey = LabeledGlobalKey(widget.text);
     super.initState();
   }
 
-  void findDropdownData()
-  {
+  void findDropdownData() {
     RenderBox renderBox = actionKey.currentContext.findRenderObject();
     height = renderBox.size.height;
     width = renderBox.size.width;
@@ -62,36 +56,89 @@ class _CustomDropdownState extends State<CustomDropdown>
     yPosition = offset.dy;
   }
 
-  OverlayEntry _createFloatingDropdown()
-  {
-    return OverlayEntry(builder: (context)
-    {
+  OverlayEntry _createFloatingDropdown() {
+    return OverlayEntry(builder: (context) {
       return Positioned(
         left: xPosition,
         width: width,
         top: yPosition + height,
         height: 2 * height + 40,
-        child: DropDownState(itemHeight: height,),
+        child: Column(
+          children: <Widget>[
+            Material(
+              elevation: 20,
+              child: Container(
+                decoration: BoxDecoration(color: Colors.black12,),
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        padding: const EdgeInsets.symmetric(horizontal:16, vertical:8),
+                        child:
+                        Column(mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Row(mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Text ("Русский"),
+                                Theme(data: ThemeData(unselectedWidgetColor: Colors.black54,),
+                                  child: Checkbox(value: isSelectedRussian,
+                                    checkColor: const Color(0xffffbe3b),
+                                    activeColor: Colors.black12,
+                                    onChanged: (bool value){
+                                      setState((){
+                                        widget.text = "Русский";
+                                        isSelectedEnglish = false;
+                                        isSelectedRussian = true;
+                                      });
+                                    },
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Row(mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Text ("English"),
+                                Theme(data: ThemeData(unselectedWidgetColor: Colors.black54,),
+                                  child: Checkbox(value: isSelectedEnglish,
+                                    checkColor: const Color(0xffffbe3b),
+                                    activeColor: Colors.black12,
+                                    onChanged: (bool value)
+                                    {
+                                      setState(()
+                                      {
+                                        widget.text = "Language";
+                                        isSelectedEnglish = true;
+                                        isSelectedRussian = false;
+                                      });
+                                    },
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+              ),
+            ),
+          ],
+        ),
       );
     });
   }
 
   @override
-  Widget build(BuildContext context)
-  {
+  Widget build(BuildContext context) {
     return GestureDetector(
       key: actionKey,
-      onTap: ()
-      {
-        setState(()
-        {
-          if (isDropdownOpened)
-          {
+      onTap: () {
+        setState(() {
+          if (isDropdownOpened) {
             floatingDropdown.remove();
-            shevron = IconData(0xe5cc, fontFamily: 'MaterialIcons', matchTextDirection: true);
+            shevron = IconData(
+                0xe5cc, fontFamily: 'MaterialIcons', matchTextDirection: true);
           }
-          else
-          {
+          else {
             findDropdownData();
             floatingDropdown = _createFloatingDropdown();
             Overlay.of(context).insert(floatingDropdown);
@@ -102,116 +149,18 @@ class _CustomDropdownState extends State<CustomDropdown>
       },
       child: Container(
           decoration:
-          BoxDecoration(color:Colors.white,),
-          padding: const EdgeInsets.symmetric(horizontal:16, vertical:8),
+          BoxDecoration(color: Colors.white,),
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
           child:
           Row
             (
               children: <Widget>[
                 Icon(shevron, color: const Color(0xffffbe3b)),
-                Text(widget.text, style: TextStyle(color: Colors.black, fontSize: 22),),
+                Text(widget.text,
+                  style: TextStyle(color: Colors.black, fontSize: 22),),
               ]
           )
       ),
-    );
-  }
-}
-
-class DropDownState extends StatefulWidget
-{
-  final double itemHeight;
-  bool isSelectedEnglish;
-  bool isSelectedRussian;
-
-  DropDownState({Key key,
-    this.itemHeight,
-    this.isSelectedEnglish = false,
-    this.isSelectedRussian = true,}): super(key: key);
-
-  @override
-  DropDown createState() => DropDown();
-}
-
-class DropDown extends State<DropDownState>
-{
-  @override
-  void initState()
-  {
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context)
-  {
-    return Column(
-      children: <Widget>[
-        Material(
-          elevation: 20,
-          child: Container(
-            decoration: BoxDecoration(color: Colors.black12,),
-            child: Column(
-              children: <Widget>[
-                Container(
-                padding: const EdgeInsets.symmetric(horizontal:16, vertical:8),
-                child:
-                  Column(mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    //Russian checkbox
-                    Row(mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Text ("Русский"),
-                        Theme(data: ThemeData(unselectedWidgetColor: Colors.black54,),
-                          child: Checkbox(value: widget.isSelectedRussian,
-                            checkColor: const Color(0xffffbe3b),
-                            activeColor: Colors.black12,
-                            onChanged: (bool value)
-                            {
-                              setState(()
-                              {
-                                widget.isSelectedEnglish = !value;
-                                widget.isSelectedRussian = value;
-                                /*Navigator.push(context,
-                                  new MaterialPageRoute(
-                                      builder: (BuildContext context) =>
-                                        new CustomDropdown(text: ("Язык"))));*/
-                              });
-                            },
-                          ),
-                        ),
-                      ],
-                    ),
-                    //English checkbox
-                    Row(mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Text ("English"),
-                        Theme(data: ThemeData(unselectedWidgetColor: Colors.black54,),
-                          child: Checkbox(value: widget.isSelectedEnglish,
-                            checkColor: const Color(0xffffbe3b),
-                            activeColor: Colors.black12,
-                            onChanged: (bool value)
-                            {
-                              setState(()
-                              {
-                                widget.isSelectedEnglish = value;
-                                widget.isSelectedRussian = !value;
-                                /*Navigator.push(context,
-                                    new MaterialPageRoute(
-                                      builder: (BuildContext context) =>
-                                        new CustomDropdown(text: ("Language"))));*/
-                              });
-                            },
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ],
     );
   }
 }
